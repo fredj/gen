a8417effa7cacf9de4f98221b9652c3ca767121c
Frederic Junod <frederic.junod@camptocamp.com>
2015-12-11 14:33:57 +0100

    Convert dates to datetime

--- basic.py
+++ basic.py
@@ -1,7 +1,15 @@
 import pandas as pd
 
+def to_datetime(data_frame, columns):
+    for column in columns:
+        data_frame[column] = pd.to_datetime(data_frame[column], errors='coerce')
+
 individus = pd.read_excel('Payerne.xls', 'Individuals', index_col=0)
 couples = pd.read_excel('Payerne.xls', 'Families', index_col=0)
+# convert columns to datetime
+to_datetime(individus, ['BIRT_DATE', 'CHR_DATE', 'DEAT_DATE'])
+to_datetime(couples, ['MARB_DATE', 'MARR_DATE'])
+
 # remove marriage with unknown mother or father
 couples = couples[(couples.MotherId != 0) & (couples.FatherId != 0)]
 
