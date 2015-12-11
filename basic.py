40ad65d8cc2f69077021ce5b847a849c46260f7e
Frederic Junod <frederic.junod@camptocamp.com>
2015-12-11 13:19:22 +0100

    Add fathers marriages

--- basic.py
+++ basic.py
@@ -2,14 +2,14 @@ import pandas as pd
 
 individus = pd.read_excel('Payerne.xls', 'Individuals', index_col=0)
 couples = pd.read_excel('Payerne.xls', 'Families', index_col=0)
-# remove marriage with unknown mother
-couples = couples[couples.MotherId != 0]
+# remove marriage with unknown mother or father
+couples = couples[(couples.MotherId != 0) & (couples.FatherId != 0)]
 
 final = individus[['Name', 'Gender', 'BIRT_DATE', 'CHR_DATE', 'DEAT_DATE']].copy()
 
 # individus[pd.notnull(individus.BIRT_DATE) & individus.BIRT_DATE.str.startswith('CAL')]
 
-# FIXME: order by date
+# mothers marriages
 for mother_id, group in couples.groupby('MotherId'):
     for index, (_, marriages) in enumerate(group.iterrows(), 1):
         columns = [('MARB_DATE', 'MARB_DATE_%d' % index),
@@ -19,8 +19,17 @@ for mother_id, group in couples.groupby('MotherId'):
                 final[final_name] = None
             final.at[mother_id, final_name] = marriages[column]
 
+# fathers marriages
+for father_id, group in couples.groupby('FatherId'):
+    for index, (_, marriages) in enumerate(group.iterrows(), 1):
+        columns = [('MARB_DATE', 'MARB_DATE_%d' % index),
+                   ('MARR_DATE', 'MARR_DATE_%d' % index)]
+        for column, final_name in columns:
+            if final_name not in final:
+                final[final_name] = None
+            final.at[father_id, final_name] = marriages[column]
 
-# FIXME: order by date
+# children births
 for family_id, children in couples['Children'].iteritems():
     if not pd.isnull(children):
         mother_id = couples.ix[family_id, 'MotherId']
