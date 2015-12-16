0077a5e6990317a4c3c46006d494fec7d51d659a
Frederic Junod <frederic.junod@camptocamp.com>
2015-12-16 16:45:22 +0100

    Cleanup

--- families.py
+++ families.py
@@ -1,9 +1,7 @@
 import datetime
 import pandas as pd
-import ggplot as gg
 import matplotlib.pyplot as plt
 import seaborn as sbn
-from scipy.stats import probplot
 from utils import read_source
 
 individus, couples = read_source()
@@ -11,14 +9,10 @@ individus, couples = read_source()
 family_children = couples[['MARB_DATE', 'MARR_DATE']].copy()
 
 # remove families without dates
-no_marb = pd.isnull(family_children.MARB_DATE)
-no_marr = pd.isnull(family_children.MARR_DATE)
-family_children = family_children[~no_marb | ~no_marr]
-
-# family_children['MARB_DATE'] - family_children['MARR_DATE']
+family_children = family_children[~family_children.MARB_DATE.isnull() | ~family_children.MARR_DATE.isnull()]
 
 # if the marriage date is unknown, the estimated date is banns + 21 days
-family_children.loc[no_marr, 'MARR_DATE'] = family_children.MARB_DATE + pd.Timedelta(days=21)
+family_children.loc[family_children.MARR_DATE.isnull(), 'MARR_DATE'] = family_children.MARB_DATE + pd.Timedelta(days=21)
 
 family_children['FIRST_CHILD'] = pd.NaT
 family_children['CHILD_NAME'] = ''
@@ -34,7 +28,7 @@ for family_id, children_ids in couples['Children'].iteritems():
             family_children.loc[family_id, 'CHILD_NAME'] = first_child['Name']
 
 # remove families without valid 'FIRST_CHILD'
-family_children = family_children[~pd.isnull(family_children.FIRST_CHILD)]
+family_children = family_children[~family_children.FIRST_CHILD.isnull()]
 
 # compute number of days between birth of the first child and the marriage date
 family_children['DIFF'] = (family_children['FIRST_CHILD'] - family_children['MARR_DATE']).dt.days
@@ -65,11 +59,12 @@ plt.figure()
 # plt.plot(bellow.index, bellow.DIFF, marker='o', color='0.5',linestyle='')
 plt.plot(final.index, final.DIFF, marker='o', color='black', alpha=0.1, linestyle='')
 #plt.plot(jomini.index, jomini.DIFF, marker='s', color='red', linestyle='')
-plt.axhline(0, linewidth=0.5, color='black', linestyle='dotted')
+plt.axhline(0, linewidth=0.75, color='black', linestyle='dotted')
 plt.axhline(nine_months, linewidth=0.5, color='black', linestyle='dotted')
 #plt.fill_between(final.index, 0, nine_months, color='0.6', alpha=0.2)
 #plt.fill_between(final.index, q10, q90, color='0.3', alpha=0.2)
-plt.plot(final.index, q50, linewidth=1, alpha=0.5)
+#plt.plot(final.index, q50, linewidth=1, alpha=0.5)
+
 plt.xlim(plt.xlim(datetime.date(1790, 1, 1), datetime.date(1855, 1, 1)))
 plt.ylim(-500, 1500)
 
@@ -82,7 +77,6 @@ sbn.despine()
 
 
 sbn.distplot(final.DIFF, rug=True)
-probplot(final.DIFF, plot=plt)
 
 sbn.kdeplot(final.index.year, final.DIFF)
 plt.xlim(1790, 1855)
