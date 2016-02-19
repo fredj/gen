f9f02161e7829a2da1143c50712614344ab00a01
Frederic Junod <frederic.junod@camptocamp.com>
2016-02-19 17:28:46 +0100

    Compute mother's and father's age at last child

--- basic.py
+++ basic.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from utils import read_source
 
 # FIXME: fix date in output xls (convert to string)
@@ -35,13 +36,30 @@ for father_id, group in couples.groupby('FatherId'):
                 final[final_name] = pd.NaT
             final.at[father_id, final_name] = marriages[column]
 
+final['CHILD_COUNT'] = np.nan
+final['AGE_AT_LAST_CHILD'] = np.nan
 # children births
 for family_id, children_ids in couples['Children'].iteritems():
     mother_id = couples.loc[family_id, 'MotherId']
+    father_id = couples.loc[family_id, 'FatherId']
     if not pd.isnull(children_ids):
         children = individus.loc[map(int, str(children_ids).split(';')), 'BIRT_DATE']
         children = children.sort_values()
         children.index = ['BIRT_DATE_CHILD_%d' % i for i in range(1, len(children.index) + 1)]
+        last_child_bith_date_index = children.last_valid_index()
+        if not pd.isnull(last_child_bith_date_index):
+            # we have at least one bith date
+            last_child_bith_date = children[last_child_bith_date_index]
+            # FIXME: test if the mother lives after this birth
+            mother_age_at_last_child = last_child_bith_date - final.loc[mother_id, 'BIRT_DATE']
+            if not pd.isnull(mother_age_at_last_child):
+                final.loc[mother_id, 'AGE_AT_LAST_CHILD'] = mother_age_at_last_child.days / 365
+
+            # FIXME: test if the father lives after this birth
+            father_age_at_last_child = last_child_bith_date - final.loc[father_id, 'BIRT_DATE']
+            if not pd.isnull(father_age_at_last_child):
+                final.loc[father_id, 'AGE_AT_LAST_CHILD'] = father_age_at_last_child.days / 365
+
         # FIXME: compute mother's age at the last birth
         final.loc[mother_id, 'CHILD_COUNT'] = len(children)
         for column, birth_date in children.iteritems():
