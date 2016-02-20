4ebe5046ed1594c8671da6a8945750089d5c913a
Frederic Junod <frederic.junod@camptocamp.com>
2016-02-20 11:07:19 +0100

    Use 'no lign pat' and 'no lign mat'

--- families.py
+++ families.py
@@ -1,8 +1,17 @@
 import datetime
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sbn
 from utils import read_source
+
+
+individus_columns = {
+    'BIRT_DATE': 'FIRST_CHILD',
+    'Name': 'CHILD_NAME',
+    'no lign pat': 'FATHER_LINE',
+    'no lign mat': 'MOTHER_LINE',
+}
 
 individus, couples = read_source()
 
@@ -14,18 +23,18 @@ family_children = family_children[~family_children.MARB_DATE.isnull() | ~family_
 # if the marriage date is unknown, the estimated date is banns + 21 days
 family_children.loc[family_children.MARR_DATE.isnull(), 'MARR_DATE'] = family_children.MARB_DATE + pd.Timedelta(days=21)
 
-family_children['FIRST_CHILD'] = pd.NaT
-family_children['CHILD_NAME'] = ''
+
 # first children birth
 for family_id, children_ids in couples['Children'].iteritems():
     if not pd.isnull(children_ids) and family_id in family_children.index:
-        children = individus.loc[map(int, str(children_ids).split(';')), ['BIRT_DATE', 'Name']]
+        children = individus.loc[map(int, str(children_ids).split(';')), individus_columns.keys()]
         children = children[~children.BIRT_DATE.isnull()]
         if len(children) > 0:
             children = children.sort_values('BIRT_DATE')
             first_child = children.iloc[0]
-            family_children.loc[family_id, 'FIRST_CHILD'] = first_child['BIRT_DATE']
-            family_children.loc[family_id, 'CHILD_NAME'] = first_child['Name']
+            # copy values from 'individus'
+            for k,v in individus_columns.iteritems():
+                family_children.loc[family_id, v] = first_child[k]
 
 # remove families without valid 'FIRST_CHILD'
 family_children = family_children[~family_children.FIRST_CHILD.isnull()]
@@ -53,7 +62,8 @@ q90 = pd.rolling_quantile(final['DIFF'], 40, 0.9)
 above = final[final.DIFF > nine_months]
 bellow = final[final.DIFF <= nine_months]
 
-jomini = final[final.CHILD_NAME.str.contains('JOMINI')]
+#final.groupby('FATHER_LINE').size().sort_values()
+group = final[final.FATHER_LINE == 73863]
 
 # %matplotlib inline
 sbn.set_style('ticks')
@@ -62,7 +72,7 @@ plt.figure()
 # plt.plot(above.index, above.DIFF, marker='o', color='0.75', linestyle='')
 # plt.plot(bellow.index, bellow.DIFF, marker='o', color='0.5',linestyle='')
 plt.plot(final.index, final.DIFF, marker='o', color='black', alpha=0.1, linestyle='')
-#plt.plot(jomini.index, jomini.DIFF, marker='s', color='red', linestyle='')
+plt.plot(group.index, group.DIFF, marker='s', color='red', linestyle='')
 plt.axhline(0, linewidth=0.75, color='black', linestyle='dotted')
 plt.axhline(nine_months, linewidth=0.5, color='black', linestyle='dotted')
 #plt.fill_between(final.index, 0, nine_months, color='0.6', alpha=0.2)
