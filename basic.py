4f761b275ea1b02cd5c5462eded887e09952fc84
Frederic Junod <frederic.junod@camptocamp.com>
2016-03-04 15:17:27 +0100

    Compute everything for every marriage (aka. big fat refactoring)

--- basic.py
+++ basic.py
@@ -22,6 +22,12 @@ def get_children_birthday(marriage):
         return children.sort_values()
     return pd.Series()
 
+
+def format_date(date):
+    # http://blog.sneawo.com/blog/2015/04/08/strftime-for-datetime-before-1900-year/
+    # date.strftime('%d/%m/%Y')
+    return '{0.day:02d}/{0.month:02d}/{0.year:4d}'.format(date)
+
 individus, couples = read_source()
 
 # remove marriage with unknown mother or father
@@ -33,6 +39,7 @@ final = individus[['Name', 'Gender', 'BIRT_DATE', 'CHR_DATE', 'ESTIM_BIRT_DATE',
 for gender_groups in [couples.groupby('MotherId'), couples.groupby('FatherId')]:
     for person_id, group in gender_groups:
         sorted_group = group.sort_values(['MARB_DATE', 'MARR_DATE'])
+        birth_date = final.loc[person_id, 'ESTIM_BIRT_DATE']
         for index, (_, marriage) in enumerate(sorted_group.iterrows(), 1):
             columns = [('MARB_DATE', 'MARB_DATE_%d' % index),
                        ('MARR_DATE', 'MARR_DATE_%d' % index)]
@@ -40,6 +47,16 @@ for gender_groups in [couples.groupby('MotherId'), couples.groupby('FatherId')]:
                 if final_name not in final:
                     final[final_name] = pd.NaT
                 final.at[person_id, final_name] = marriage[column]
+
+            # column = 'MARR_ID_%d' % index
+            # if column not in final:
+            #     final[column] = ''
+            # final.at[person_id, column] = marriage.index
+
+            # column = 'MARR_TYPE_%d' % index
+            # if column not in final:
+            #     final[column] = ''
+            # final.at[person_id, column] = marriage['_UST']
 
             union_date = get_union_date(marriage)
             column = 'MARR_CALC_%d' % index
@@ -50,72 +67,53 @@ for gender_groups in [couples.groupby('MotherId'), couples.groupby('FatherId')]:
             column = 'AGE_MARR_%d' % index
             if column not in final:
                 final[column] = np.nan
-            birth_date = final.loc[person_id, 'ESTIM_BIRT_DATE']
             if not pd.isnull(union_date) and not pd.isnull(birth_date):
                 final.at[person_id, column] = (union_date - birth_date).days / 365
 
-            column = 'DAYS_BEFORE_FIRST_CHILD_%d' % index
             children_birthday = get_children_birthday(marriage)
             first_child_index = children_birthday.first_valid_index()
+            last_child_index = children_birthday.last_valid_index()
+            children_birthday_diff = children_birthday.diff()
+
+            column = 'CHILD_COUNT_%d' % index
+            if column not in final:
+                final[column] = np.nan
+            final.at[person_id, column] = len(children_birthday)
+
+            column = 'DIFF_CHILD_%d' % index
+            if column not in final:
+                final[column] = ''
+            final.at[person_id, column] = ' '.join([str(d.days) for d in children_birthday_diff if not pd.isnull(d)])
+
+            column = 'MEAN_DIFF_CHILD_%d' % index
+            if column not in final:
+                final[column] = np.nan
+            children_birthday_diff_mean = children_birthday_diff.mean()
+            if not pd.isnull(children_birthday_diff_mean):
+                final.at[person_id, column] = children_birthday_diff_mean.days
+
+            column = 'DAYS_BEFORE_FIRST_CHILD_%d' % index
             if column not in final:
                 final[column] = np.nan
             if not pd.isnull(union_date) and not pd.isnull(first_child_index):
                 final.at[person_id, column] = (children_birthday.loc[first_child_index] - union_date).days
 
-
-final['CHILD_COUNT'] = np.nan
-final['AGE_AT_LAST_CHILD'] = np.nan
-final['DIFF_CHILD'] = ''
-final['MEAN_DIFF_CHILD'] = np.nan
-# children births
-for family_id, children_ids in couples['Children'].iteritems():
-    mother_id = couples.loc[family_id, 'MotherId']
-    father_id = couples.loc[family_id, 'FatherId']
-    if not pd.isnull(children_ids):
-        children = individus.loc[map(int, str(children_ids).split(';')), 'BIRT_DATE']
-        children = children.sort_values()
-        children.index = ['BIRT_DATE_CHILD_%d' % i for i in range(1, len(children.index) + 1)]
-        last_child_birth_date_index = children.last_valid_index()
-        if not pd.isnull(last_child_birth_date_index):
-            # we have at least one birth date
-            last_child_birth_date = children[last_child_birth_date_index]
-            for person_id in [mother_id, father_id]:
-                # FIXME: test if the person lives after this birth
-                person_age_at_last_child = last_child_birth_date - final.loc[person_id, 'ESTIM_BIRT_DATE']
-                if not pd.isnull(person_age_at_last_child):
-                    final.loc[person_id, 'AGE_AT_LAST_CHILD'] = person_age_at_last_child.days / 365
-
-        final.loc[mother_id, 'CHILD_COUNT'] = len(children)
-        for column, birth_date in children.iteritems():
+            column = 'AGE_AT_LAST_CHILD_%d' % index
             if column not in final:
-                final[column] = pd.NaT
-            final.loc[mother_id, column] = birth_date
+                final[column] = np.nan
+            if not pd.isnull(last_child_index) and not pd.isnull(birth_date):
+                # we have at least one birth date
+                last_child_birth_date = children_birthday[last_child_index]
+                # FIXME: test if the person lives after this birth
+                person_age_at_last_child = last_child_birth_date - birth_date
+                if not pd.isnull(person_age_at_last_child):
+                    final.loc[person_id, column] = person_age_at_last_child.days / 365
 
-        final.loc[mother_id, 'DIFF_CHILD'] = ','.join([str(d.days) for d in children.diff() if not pd.isnull(d)])
-        # mean time between births
-        mean_diff_child = children.diff().mean()
-        if not pd.isnull(mean_diff_child):
-            final.loc[mother_id, 'MEAN_DIFF_CHILD'] = mean_diff_child.days
-    else:
-        final.loc[mother_id, 'CHILD_COUNT'] = 0
+            column = 'CHILDREN_BIRTHDAY_%d' % index
+            if column not in final:
+                final[column] = ''
+            final.at[person_id, column] = ' '.join([format_date(d) for d in children_birthday if not pd.isnull(d)])
+
 
 writer = pd.ExcelWriter('final.xls',  datetime_format='DD/MM/YYYY')
 final.to_excel(writer, sheet_name='Individuals')
-
-
-by_year = {
-    'Illegitimate': final[(final.DAYS_BEFORE_FIRST_CHILD_1 < 0)],
-    'Pre-nuptial': final[(final.DAYS_BEFORE_FIRST_CHILD_1 > 0) & (final.DAYS_BEFORE_FIRST_CHILD_1 < 274)],
-}
-
-for name, data_frame in by_year.items():
-    data_frame_by_year = data_frame.groupby(data_frame.MARR_CALC_1.dt.year).count()
-    data_frame_by_year = data_frame_by_year[['DAYS_BEFORE_FIRST_CHILD_1']]
-    # divide value by two because we have one entry for the father and one for the mother
-    data_frame_by_year = data_frame_by_year.apply(lambda count: count / 2)
-    data_frame_by_year.columns = [name]
-    data_frame_by_year.to_excel(writer, sheet_name=name)
-
-
-writer.save()
-writer.close()
